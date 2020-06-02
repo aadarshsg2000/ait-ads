b1e46961c250615146bbb2b37addad5ace7734ef
aadarshsg2000 <sgaadarshinfotech@gmail.com>
2020-06-02 19:15:44 +0530

    Website made temporarily

--- mail.php
+++ mail.php
@@ -2,11 +2,14 @@
   $name = $_POST['name'];
   $visitor_email = $_POST['email'];
   $message = $_POST['message'];
-
+  $subject = $_POST['subject'];
+  $number = $_POST['number'];
   $email_from = 'sgaadarshinfotech@gmail.com';
   $email_subject = "New Form Submission";
   $email_body = "User Name: $name. \n".
                 "User Email: $visitor_email. \n".
+                "User No: $number. \n".
+                "User Sub: $subject. \n".
                 "User Message: $message. \n";
 
   $to = "sgaadarshinfotech@gmail.com";
@@ -14,4 +17,5 @@
   $headers .= "Reply-To: $visitor_email \r\n";
   mail($to,$email_subject,$email_body,$headers);
   header("Location: contact.html");
+  echo "Your details has send successfully";
 ?>
